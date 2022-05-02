0a5f276b17fc794b524d1c2dec69a1cf547a139d
Denes <jose.araujo@zup.com.br>
2022-05-02 09:05:37 -0300

    commit inicial

---
+++ src/main/java/br/com/zup/edu/loteria/LoteriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.loteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoteriaApplication.class, args);
+	}
+
+}
